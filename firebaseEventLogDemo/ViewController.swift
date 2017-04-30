68dfe68ea24343eca39675d9fa086753aa1c9a05
stanchiang <stanchiang23@gmail.com>
2017-04-30 13:01:07 -0700

    init proj; remove main storyboard

---
+++ firebaseEventLogDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebaseEventLogDemo
+//
+//  Created by Stanley Chiang on 4/30/17.
+//  Copyright © 2017 Stanley Chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
